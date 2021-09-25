b09cc741b66fd47910f1a26abbaed7bbef5d36bb
rakeshghumatkar <88374586+rakeshghumatkar@users.noreply.github.com>
2021-09-25 18:00:05 +0530

    mySql connection and apis called

---
+++ spring_mysql_conn/src/test/java/com/example/demo/mysql_conn/SpringMysqlConnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.mysql_conn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlConnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
